99564f120092833c08f5d35402cfa794c2fb9aca
lumbric <lumbric@gmail.com>
2022-10-25 10:18:16 +0200

    Refactor tests
    Remove a test, parametrize the other one instead.

--- test_network.py
+++ test_network.py
@@ -14,6 +14,8 @@ from network import NUM_TIME_STEPS
 
 
 def test_expensive_solar_pv():
+    """If PV is more expensive than wind, the model should choose only wind (for constant wind/PV
+     profiles)."""
     def const_time_series(value):
         return xr.DataArray(
             value * np.ones(NUM_TIME_STEPS),
@@ -44,47 +46,14 @@ def test_expensive_solar_pv():
     assert system.model.solution.size_solar_pv == 0.0
 
 
-def test_simple_co2():
-    # %%
+@pytest.mark.parametrize("with_storage", [False, True])
+def test_simple_co2_storage(with_storage):
+    """A simple methanol synthesis network with wind only to produce hydrogen.
 
-    wind_flow = xr.DataArray(
-        0.5 * np.ones(NUM_TIME_STEPS),
-        dims="time",
-        coords={"time": np.arange(NUM_TIME_STEPS)},
-    )
-    co2_flow = xr.DataArray(
-        0.5 * np.ones(NUM_TIME_STEPS),
-        dims="time",
-        coords={"time": np.arange(NUM_TIME_STEPS)},
-    )
-
-    wind = NodeFixInput(name="wind", input_flow=wind_flow, costs=1)
-    hydrogen = Node(name="hydrogen", inputs=[wind], costs=3)
-    co2 = NodeFixInput(name="co2", input_flow=co2_flow)
-
-    methanol_synthesis = Node(
-        name="methanol_synthesis",
-        inputs=[co2, hydrogen],
-        costs=8e-6,
-        input_proportions={"co2": 0.25, "hydrogen": 0.75},
-    )
-
-    system = System([wind, hydrogen, co2, methanol_synthesis])
-    system.optimize("gurobi")
-
-    assert system.model.solution.size_wind == 3.0
-    assert system.model.solution.size_hydrogen == 1.5
-    assert system.model.solution.size_methanol_synthesis == 2.0
-    assert np.all(system.model.solution.flow_wind_hydrogen == 1.5)
-    assert np.all(system.model.solution.flow_co2_methanol_synthesis == 0.5)
-    assert np.all(system.model.solution.flow_hydrogen_methanol_synthesis == 1.5)
-    assert np.all(system.model.solution.flow_methanol_synthesis == 2.0)
-
-    # %%
-
-
-def test_simple_co2_storage():
-    # %%
+    The optimum should be identical in both scenarios:
+     - there is a constant 0.5 CO2 input flow (no storage)
+     - CO2 input flow is alternating between 0 and 1 and there is a CO2 storage
+    """
 
     wind_flow = xr.DataArray(
         0.5 * np.ones(NUM_TIME_STEPS),
@@ -94,11 +63,14 @@ def test_simple_co2_storage():
     co2_flow = xr.DataArray(
         np.ones(NUM_TIME_STEPS), dims="time", coords={"time": np.arange(NUM_TIME_STEPS)}
     )
-    co2_flow[1::2] = 0
-
-    storage = Storage(
-        costs=1000, max_charging_speed=1.0, storage_loss=0.0, charging_loss=0.0
-    )
+    if with_storage:
+        co2_flow[1::2] = 0
+        storage = Storage(
+            costs=1000, max_charging_speed=1.0, storage_loss=0.0, charging_loss=0.0
+        )
+    else:
+        co2_flow = 0.5 * co2_flow
+        storage = None
 
     wind = NodeFixInput(name="wind", input_flow=wind_flow, costs=1)
     hydrogen = Node(name="hydrogen", inputs=[wind], costs=3)
@@ -114,7 +86,6 @@ def test_simple_co2_storage():
     system = System([wind, hydrogen, co2, methanol_synthesis])
     system.optimize("gurobi")
 
-    # %%
     np.testing.assert_almost_equal(system.model.solution.size_wind, 3.0)
     np.testing.assert_almost_equal(system.model.solution.size_hydrogen, 1.5)
     np.testing.assert_almost_equal(system.model.solution.size_methanol_synthesis, 2.0)
@@ -122,5 +93,3 @@ def test_simple_co2_storage():
     np.testing.assert_array_almost_equal(system.model.solution.flow_co2_methanol_synthesis, 0.5)
     np.testing.assert_array_almost_equal(system.model.solution.flow_hydrogen_methanol_synthesis, 1.5)
     np.testing.assert_array_almost_equal(system.model.solution.flow_methanol_synthesis, 2.0)
-
-    # %%
