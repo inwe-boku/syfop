e75a0a0eaf1640db0d5da6467ca1b23df04eac7e
lumbric <lumbric@gmail.com>
2024-05-07 19:13:54 +0200

    Fetch complete git history on Github actions
    This should allow installing syfop via pip and fix the issues with
    version.py.

--- syfop/version.py
+++ syfop/version.py
@@ -4,7 +4,16 @@ import subprocess
 def get_version():
     """Get the latest Git tag and prepend 'v' and use the Git comit hash as suffix if the current
     commit has not Git tag."""
-    git_tag = subprocess.check_output(["git", "describe", "--tags"]).decode().strip()
+    # TODO this will raise an exception if:
+    #  - there are no tags
+    #  - if it is a shallow clone  with --depth 1
+    try:
+        git_tag = subprocess.check_output(["git", "describe", "--tags"], stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(f"Unable to get version from Git: {e}: {e.output.decode()}")
+
+    # TODO we don't check if the tag is a valid version string (e.g. v0.1.0).
+    git_tag = git_tag.decode().strip()
     version = git_tag.lstrip("v")
 
     # convert string from git-describe to a valid PEP440 version string
